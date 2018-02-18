0858e9959778ef86095fdf20711f388403c58565
Raziel <3615018+rraziel@users.noreply.github.com>
2018-02-18 10:28:38 +0100

    Missing FutureOrPresent decorator

---
+++ src/decorators/future-or-present.ts
@@ -0,0 +1,13 @@
+import {withInfoBuilder, ValidationDecorator} from './helper';
+
+/**
+ * FutureOrPresent decorator, used to define that an element must be a date in the future or the present
+ * @param target      Target
+ * @param propertyKey Property key
+ * @param descriptor  Descriptor
+ */
+const FutureOrPresent: ValidationDecorator = (target, propertyKey, descriptor) => withInfoBuilder(target, propertyKey, descriptor, infoBuilder => infoBuilder.future(true));
+
+export {
+    FutureOrPresent
+};
