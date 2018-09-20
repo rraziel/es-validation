7310df6135f63588c2a780f41a5a5d81e302537d
Raziel <3615018+rraziel@users.noreply.github.com>
2018-09-20 06:47:15 +0200

    Improved test coverage for the Size decorator

--- packages/decorators/src/decorators/Size.spec.ts
+++ packages/decorators/src/decorators/Size.spec.ts
@@ -4,6 +4,10 @@ import { Size } from './Size';
 
 describe('@Size decorator', () => {
 
+    beforeEach(() => {
+        addConstraint.mockReset();
+    });
+
     it('adds a constraint', () => {
         // given
         let target: object|Function = function(): void { /* empty */ };
@@ -15,5 +19,25 @@ describe('@Size decorator', () => {
         expect(addConstraint).toHaveBeenCalledWith({target, propertyKey, descriptor: undefined, constraintName: 'Size', attributes: {mininum: 2, maximum: 4}});
     });
 
-    // TODO: minimum & maximum must be >= 0
+    it('adds a constraint with default values', () => {
+        // given
+        let target: object|Function = function(): void { /* empty */ };
+        let propertyKey: string|symbol = 'test';
+        // when
+        Size()(target, propertyKey, undefined);
+        // then
+        expect(addConstraint).toHaveBeenCalledTimes(1);
+        expect(addConstraint).toHaveBeenCalledWith({target, propertyKey, descriptor: undefined, constraintName: 'Size', attributes: {mininum: 0, maximum: Number.MAX_SAFE_INTEGER}});
+    });
+
+    it('rejects negative values', () => {
+        // given
+        let target: object|Function = function(): void { /* empty */ };
+        let propertyKey: string|symbol = 'test';
+        // expect
+        expect(() => {
+            Size(-1, -2)(target, propertyKey, undefined);
+        }).toThrowError('Decorator @Size cannot have a negative parameters (minimum: -1, maximum: -2)');
+    });
+
 });
--- packages/decorators/src/decorators/Size.ts
+++ packages/decorators/src/decorators/Size.ts
@@ -9,6 +9,10 @@ import { ConstraintProperties } from './ConstraintProperties';
  * @return Size decorator
  */
 function Size(minimum: number = 0, maximum: number = Number.MAX_SAFE_INTEGER): ConstraintDecorator {
+    if (minimum < 0 || maximum < 0) {
+        throw new Error(`Decorator @Size cannot have a negative parameters (minimum: ${minimum}, maximum: ${maximum})`);
+    }
+
     return <T>(target, propertyKey, descriptor) => {
         let constraintProperties: ConstraintProperties<T> = new ConstraintProperties<T>(target, propertyKey, descriptor, 'Size');
 
